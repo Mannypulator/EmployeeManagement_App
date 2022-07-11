8c4fc8d4d001e9ad8b705a52877635ce4fc83459
mannypulator <emeritus93@gmail.com>
2022-07-11 20:18:41 +0100

    added exception handling and 404 page

---
+++ Controllers/ErrorController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagement.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // If there is 404 status code, the route path will become Error/404
+        [Route("Error/{statusCode}")]
+        public IActionResult HttpStatusCodeHandler(int statusCode)
+        {
+            switch (statusCode)
+            {
+                case 404:
+                    ViewBag.ErrorMessage = "Sorry, the resource you requested could not be found";
+                    break;
+            }
+
+            return View("NotFound");
+        }
+        [Route("Error")]
+        [AllowAnonymous]
+        public IActionResult Error()
+        {
+            var exceptionDetails = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+            ViewBag.ExceptionPath = exceptionDetails.Path;
+            ViewBag.ExceptionMessage = exceptionDetails.Error.Message;
+            ViewBag.StackTrace = exceptionDetails.Error.StackTrace;
+            return View("Error");
+        }
+    }
+
+}
