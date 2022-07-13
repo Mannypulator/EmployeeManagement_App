0dfa31f6e1cc6fd579612da7bb7539836a6e7e57
mannypulator <emeritus93@gmail.com>
2022-07-13 01:34:39 +0100

    enforced on delete no action implemented

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -15,6 +15,10 @@ namespace EmployeeManagement.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+            foreach (var foreignKey in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                foreignKey.DeleteBehavior = DeleteBehavior.Restrict;
+            }
             modelBuilder.Seed();
         }
     }
